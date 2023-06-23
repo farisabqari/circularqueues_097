fe8fb6f996e4edfcea829d05d82a1684eb3cc06d
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-06-23 16:35:49 +0800

    menambahkan void display

--- circularqueue/circularqueue.cpp
+++ circularqueue/circularqueue.cpp
@@ -33,5 +33,30 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
-	
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT = -1) {
+			cout << "Queue ubderflow\n";
+			return;
+
+		}
+		cout << "\nThe element deleted from the wuque is:" << queue_array[FRONT] << "\n";
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			FRONT = -1;
+			REAR = -1;
+
+		}
+		else {
+			//jika element yang dihapus berada di posisi terakhirarray, kembali ke awal
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+	void display() {
+
+	}
 };
