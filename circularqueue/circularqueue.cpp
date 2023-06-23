f3f745d0262e3e26eda90815ab190733a5c026c1
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-06-23 16:25:06 +0800

    mengecek apakah antrian penuh

--- circularqueue/circularqueue.cpp
+++ circularqueue/circularqueue.cpp
@@ -14,6 +14,11 @@ public:
 		cout << "Enter a nuber:";
 		cin >> num;
 		cout << endl;
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
 	}
 
 };
