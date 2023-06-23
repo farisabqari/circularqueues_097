150447a8ead20f9d8f916db7b31142da30ea7e1c
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-06-23 16:31:58 +0800

    jika rear berada di posisi terakhir array maka kembali ke awal array

--- circularqueue/circularqueue.cpp
+++ circularqueue/circularqueue.cpp
@@ -25,6 +25,11 @@ public:
 			REAR = 0;
 		}
 		else {
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
 		}
 	}
 
